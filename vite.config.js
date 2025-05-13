869188ba5130aee7023be6c5e576e8acc60cfb84
ZhuravlevaAnastasia <nas20040908@gmail.com>
2025-05-13 18:54:55 +0300

    Use relative base for assets (base='./')

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  publicDir: false,     // Отключаем папку public, чтобы Vite не искал шаблоны там
-  root: '.',            // Корень проекта
-  build: {
-    outDir: 'dist'
-  }
-})
+export default defineConfig(({ command }) => ({
+  base: command === 'serve' ? '/' : '/dog-app-react/',
+  plugins: [react()]
+}))
